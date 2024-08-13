f48deebf8c7aa92dc40ffc49afff62f44101385f
Captain <diana.iminza@ronforddigital.com>
2024-08-13 14:28:35 +0300

    add controller

---
+++ web-api-google-authentication/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace web_api_google_authentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            var props = new AuthenticationProperties { RedirectUri = "account/signin-google" };
+            return Challenge(props, GoogleDefaults.AuthenticationScheme);
+        }
+        [HttpGet("signin-google")]
+        public async Task<IActionResult> GoogleLogin()
+        {
+            var response = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            if (response.Principal == null) return BadRequest();
+
+            var name = response.Principal.FindFirstValue(ClaimTypes.Name);
+            var givenName = response.Principal.FindFirstValue(ClaimTypes.GivenName);
+            var email = response.Principal.FindFirstValue(ClaimTypes.Email);
+            //Do something with the claims
+            //var user = await UserService.FindOrCreate(new { name, givenName, email});
+
+            return Ok();
+        }
+    }
+}

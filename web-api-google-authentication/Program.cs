7c7a44f47236947691a8b771b211d11f6cb263a2
Captain <diana.iminza@ronforddigital.com>
2024-08-13 14:26:55 +0300

    install authenticator

--- web-api-google-authentication/Program.cs
+++ web-api-google-authentication/Program.cs
@@ -1,15 +1,29 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = "YOUR_GOOGLE_CLIENT_ID";
+    options.ClientSecret = "YOUR_GOOGLE_CLIENT_SECRET";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
